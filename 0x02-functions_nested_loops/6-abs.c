df1ea97330e9c8a67f69c22257245687a87663b0
EdnaNkatha <ednankatha33@gmail.com>
2022-07-14 12:22:33 -0700

    all tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * _abs - Compute absolute value of a number
+ *
+ *   *@k: integer
+ *
+ *    *
+ *
+ *     * Return: Always 0 (Success)
+ *
+ *      */
+
+int _abs(int k)
+
+{
+
+		if (k >= 0)
+
+					return (k);
+
+			k *= -1;
+
+				return (k);
+
+}
